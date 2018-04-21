4fc3c18602adbb94d25d07b55f3f779c64bba376
ML <emele18@gmail.com>
2018-04-21 14:55:02 -0400

    doc2vec extracted

--- doc2vec.py
+++ doc2vec.py
@@ -1,20 +1,37 @@
 from gensim.models.doc2vec import Doc2Vec
 import json
+import gensim
+import gensim.utils as ut
+from collections import namedtuple
+import random
 
-def read_documents(docjson):
-    with open(docjson) as f:
-        documents = json.load(f)
+ArtistDocument = namedtuple('ArtistDocument', 'words tags index')
+
+def read_documents(documents, stemmer=0):
+    alldocs = []
+    allindex={}
 
     for doc in documents:
         text = documents[doc]
-        print doc
-        print text
-        exit()
 
+        if stemmer == 1:
+            text = gensim.parsing.stem_text(text)
+        else:
+            text = text.lower()
+
+        words = ut.to_unicode(text).split()
+        tags = [doc] # ID of each document for doc2vec model
+        index = len(alldocs)
+
+        allindex[doc] = index # mapping from doc ID to index, start from 0
+        alldocs.append(ArtistDocument(words, tags, index))
+
+    return alldocs, allindex
 
 def train_doc2vec(doc_list=None, buildvoc=1, passes=20, dm=0,
                  size=100, dm_mean=0, window=5, hs=1, negative=5,
-                 min_count=1, workers=4):
+                 min_count=1, workers=4, random_state=42):
+    random.seed(random_state)
     model = Doc2Vec(dm=dm, size=size, dm_mean=dm_mean, window=window,
                     hs=hs, negative=negative, min_count=min_count, workers=workers) #PV-DBOW
     if buildvoc == 1:
@@ -23,20 +40,52 @@ def train_doc2vec(doc_list=None, buildvoc=1, passes=20, dm=0,
 
     for epoch in range(passes):
         print('Iteration %d ....' % epoch)
-        shuffle(doc_list)  # shuffling gets best results
+        random.shuffle(doc_list)  # shuffling gets best results
 
         model.train(doc_list)
 
     return model
 
 def main():
+    embedding = {}
+
+    # some parameters
     numFea = 300
     cores = 4
     random_state = 42
+    passes = 100
+    dm = 0
 
-    read_documents("summaries.json")
+    # Load documents into memory
+    with open("summaries.json") as f:
+        documents = json.load(f)
 
-    #doc2vec_model = net.trainDoc2Vec(doc_list, workers=cores, size=numFea, dm=dm, passes=passes, min_count=3)
+    print "Artists found:", len(documents)
 
+    alldocs, allindex = read_documents(documents)
+    doc_list = alldocs[:] # for future reshuffling
+
+    doc2vec_model = train_doc2vec(doc_list, workers=cores, size=numFea,
+                                  dm=dm, passes=passes, min_count=3,
+                                  random_state=42)
+
+    for doc in doc_list:
+        vector = doc2vec_model.docvecs[doc.tags[0]]
+        embedding[doc.tags[0]] = vector
+
+    dummy_key = embedding.keys()[0]
+    f_doc2vec = open('doc2vec.bin', 'wb')
+    f_doc2vec.write(str(len(embedding)))
+    f_doc2vec.write(' ')
+    f_doc2vec.write(str(len(embedding[dummy_key])))
+    f_doc2vec.write('\n')
+
+    for key in embedding:
+        f_doc2vec.write(key)
+        f_doc2vec.write(' ')
+        f_doc2vec.write(embedding[key].tobytes())
+        f_doc2vec.write('\n')
+
+    f_doc2vec.close()
 if __name__ == "__main__":
     main()
